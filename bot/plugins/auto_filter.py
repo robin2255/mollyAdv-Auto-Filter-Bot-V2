7c9359129008169e663f002914a4cb6ec48e97d1
robin2255 <83762091+robin2255@users.noreply.github.com>
2021-05-29 18:54:18 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -139,7 +139,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⭕️ PAGE 1/{len_result if len_result < max_pages else max_pages}⭕️", callback_data="ignore")
         ])
         
         result[0].append([ InlineKeyboardButton(f"🔘 GET OUR ALL CHANNELS 🔘", url="https://t.me/mollywoodchannels") ])        
